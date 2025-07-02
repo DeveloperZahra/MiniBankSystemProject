74baa62d3d1e79b67474e41ddf5b2ac1b267e672
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-02 14:13:38 +0400

    Mini Bank System Project

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Mini_Bank_System_Project
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//____________Using Directives__________
+using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Net.Http.Headers;
+using System.Numerics;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//______________Namespace Declaration__________
