53709f06d9971dc4bb8d012bd8ded395f1ed6da5
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-02 14:18:14 +0400

    Constants

--- Program.cs
+++ Program.cs
@@ -16,3 +16,17 @@ using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 //______________Namespace Declaration__________
+namespace MiniBankSystemProjectOverview
+{
+    //___________Class Declaration________
+    class Program
+    {
+        // _______Constants_________
+        const double MinimumBalance = 100.0;
+        const string AccountsFilePath = "accounts.txt";
+        const string ReviewsFilePath = "reviews.txt";
+        const string RequestsFilePath = "requests.txt";
+        const string InAcceptRequestsFilePath = "InRequests.txt";
+        const string AdminInformationFilePath = "Admin.txt";
+        private static readonly char adminChoice;
+        private static readonly bool inAdminMenu;
